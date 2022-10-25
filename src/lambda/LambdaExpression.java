7bc9f77fac7df5fbbd4cf532426e1842ffe5cea8
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-10-25 18:20:47 +0800

    lambda expressions body

--- src/lambda/LambdaExpression.java
+++ src/lambda/LambdaExpression.java
@@ -11,6 +11,18 @@ public class LambdaExpression {
 
         Tester tester = (val) -> val.length()>4;
         System.out.println(tester.test("hey"));
+        Tester tester1 = val -> {
+            String s = val.substring(0,val.length()-2);
+            if(s.length()>2){
+                System.out.println(val.length());
+                System.out.println(s);
+                return true;
+            }
+            else{
+                return false;
+            }
+        };
+        System.out.println(tester1.test("macaroni"));
 
     }
 }
