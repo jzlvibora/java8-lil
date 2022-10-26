bf4b39e653b14c06842500fea1de667636b26103
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-10-26 16:09:00 +0800

    creating stream

---
+++ src/streamsapi/CreateStream.java
@@ -0,0 +1,26 @@
+package streamsapi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CreateStream {
+    public static void main(String[] args) {
+        String[] names = {"Ana", "Cave", "Chime","Victor"};
+        List<String> nameList = new ArrayList<>(Arrays.asList(names));
+
+        //create stream based on array
+        Stream<String> arrayStream = Arrays.stream(names);
+
+        //craete stream based on collection
+        Stream<String> listStream = nameList.stream();
+
+        //create stream using generate method
+        Stream<Integer> generateStream =Stream.generate(()->5);
+
+        //create stream using iterate method
+        Stream<Integer> iterateStream = Stream.iterate(1, x->x+2);
+
+    }
+}
